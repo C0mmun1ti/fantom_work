56da573f1c1ccd5c8fe1786849fb2df77f2cab09
Egor <egorkononec64@gmail.com>
2024-05-01 13:41:40 +0300

    change gitignore and googleManage

--- classes/google_manage.py
+++ classes/google_manage.py
@@ -9,22 +9,23 @@ from ast import literal_eval  # распаршивает строку в дик�
 dotenv.load_dotenv()
 
 class GoogleSheetManager:
+    CREDENTIALS = os.getenv("CREDENTIALS")  # ебанутый словарь с какой-то хуйнёи гугловской
+    _key = os.getenv("GOOGLE_SHEET_KEY")
+    gc = gspread.service_account_from_dict(literal_eval(CREDENTIALS))
+    worksheet = gc.open_by_key(_key)
+
     def __init__(self):
-        CREDENTIALS = os.getenv("CREDENTIALS")  # ебанутый словарь с какой-то хуйнёи гугловской
-        _key = os.getenv("GOOGLE_SHEET_KEY")
-        gc = gspread.service_account_from_dict(literal_eval(CREDENTIALS))
-        worksheet = gc.open_by_key(_key)
-        self.sheet_p2p = worksheet.get_worksheet(0)
-        self.sheet_sbp = worksheet.get_worksheet(1)
+        self.sheet_p2p = self.worksheet.get_worksheet(0)
+        self.sheet_sbp = self.worksheet.get_worksheet(1)
 
     def get_data(self, p2p_or_sbp: str):
-        if p2p_or_sbp == "sbp":
+        if p2p_or_sbp.lower() == "sbp":
             return self.sheet_sbp.get('')[1:]
         return self.sheet_p2p.get('')[1:]
 
     def update_data(self, p2p_or_sbp: str, data_: dict) -> None:
-        example_list = [k for v, k in data_.items()]
-        if p2p_or_sbp == 'sbp':
+        example_list = list(data_.values())
+        if p2p_or_sbp.lower() == 'sbp':
             self.sheet_sbp.append_rows([example_list])
             return
         self.sheet_p2p.append_rows([example_list])
